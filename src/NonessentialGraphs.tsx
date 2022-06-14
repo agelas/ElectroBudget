38a823dae8082593a6a8c332f4994e39d1801217
Mathias <mathiaswi177@gmail.com>
2022-06-13 19:40:15 -0700

    new graph added and more sections added

--- src/NonessentialGraphs.tsx
+++ src/NonessentialGraphs.tsx
@@ -1,5 +1,6 @@
 import { Grid, Paper } from '@mantine/core';
 import IncomeLineGraph from './IncomeLineGraph';
+import IncomeStackBar from './IncomeStackBar';
 
 export default function NonessntialGraphs() {
     return (
@@ -17,7 +18,7 @@ export default function NonessntialGraphs() {
                 sx={(theme) => ({
                     backgroundColor: theme.colors.dark[4]
                 })}>
-                    2
+                    <IncomeStackBar />
                 </Paper>
             </Grid.Col>
             <Grid.Col span={3}>
@@ -26,6 +27,30 @@ export default function NonessntialGraphs() {
                     backgroundColor: theme.colors.dark[4]
                 })}>
                     3
+                </Paper>
+            </Grid.Col>
+            <Grid.Col span={4}>
+            <Paper radius="md" p="md"
+                sx={(theme) => ({
+                    backgroundColor: theme.colors.dark[4]
+                })}>
+                    4
+                </Paper>
+            </Grid.Col>
+            <Grid.Col span={4}>
+            <Paper radius="md" p="md"
+                sx={(theme) => ({
+                    backgroundColor: theme.colors.dark[4]
+                })}>
+                    5
+                </Paper>
+            </Grid.Col>
+            <Grid.Col span={4}>
+            <Paper radius="md" p="md"
+                sx={(theme) => ({
+                    backgroundColor: theme.colors.dark[4]
+                })}>
+                    6
                 </Paper>
             </Grid.Col>
         </Grid>
