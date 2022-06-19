aa72a6797e5f5863aef1e63f2a8103cfd5cd52ed
Mathias <mathiaswi177@gmail.com>
2022-06-19 01:04:54 -0700

    ExpenseGroup display responsive to type of expenditure

--- src/CostStack.tsx
+++ src/CostStack.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import { Stack, Button } from '@mantine/core';
+import { Stack } from '@mantine/core';
+import ExpenseGroup from "./ExpenseGroup";
 
 export default function CostStack() {
     return(
         <Stack spacing="lg" sx={(theme) => ({ backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[0], height: 300})}>
-            <Button variant="outline">1</Button>
-            <Button variant="outline">2</Button>
-            <Button variant="outline">3</Button> 
-            <Button variant="outline">1</Button>
-            <Button variant="outline">2</Button>
-            <Button variant="outline">3</Button> 
+            <ExpenseGroup expenseType= 'nonessential'/>
+            <ExpenseGroup expenseType='essentialRecurring' />
         </Stack>
     )
 }
--- src/ExpenseGroup.tsx
+++ src/ExpenseGroup.tsx
@@ -1,7 +1,24 @@
-import { Group, ActionIcon, Text, Paper } from "@mantine/core";
+import React from "react";
+import { Group, ActionIcon, Text, Paper, Switch } from "@mantine/core";
 import { Coffee } from "tabler-icons-react";
 
-export default function ExpenseGroup() {
+interface ExpenseGroupProps {
+    expenseType: string;
+}
+
+function NonEssentialToggle() {
+    return(
+        <Switch color='cyan' label='Purchased' />
+    )
+}
+
+function EssentialRecurringToggle() {
+    return(
+        <Switch color='cyan' label="Active"  />
+    )
+}
+
+export default function ExpenseGroup({expenseType} : ExpenseGroupProps) {
     return(
         <Paper radius="md" p="xs">
             <Group position='apart'>
@@ -14,6 +31,8 @@ export default function ExpenseGroup() {
                     </Text>
                 </Group>
                 <Group position='right'>
+                    {expenseType === 'nonessential' && <NonEssentialToggle />}
+                    {expenseType === 'essentialRecurring' && <EssentialRecurringToggle />}
                     <Text>
                         $620.00
                     </Text>
