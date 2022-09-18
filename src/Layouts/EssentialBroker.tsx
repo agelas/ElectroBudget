fb47b5d3489aedd6ca716cdf9b65fb1e613186a8
Mathias <mathiaswi177@gmail.com>
2022-09-18 01:18:53 -0700

    Put in bar graph and ring graph

--- src/Layouts/EssentialBroker.tsx
+++ src/Layouts/EssentialBroker.tsx
@@ -1,9 +1,40 @@
 import { Space } from "@mantine/core";
 import EssentialInfo, { InfoProps } from "./EssentialInfo";
 import EssentialDisplays from "./EssentialDisplays";
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
+import { DisplayData } from "./NonessentialDisplays";
+import { fetchData } from "../Utils/FetchData";
 
 export default function EssentialBroker(): ReactElement<any, any> {
+
+  const [data, setData] = useState<any>([]);
+  const [dataLoaded, setDataLoaded] = useState<boolean>(false);
+  const [graphData, setGraphData] = useState<DisplayData>({graphData: []});
+
+  //First one fetches and sets the data
+  useEffect( () => {
+      (async () => {
+          setDataLoaded(false);
+          let res = await fetchData(setData);
+          if (res.success) {
+              console.log(res.data);
+              setData(res.data)
+              setDataLoaded(true);
+          }
+      })();
+      
+  }, [])
+
+  //Second one fills in the data and sets the right variables
+  useEffect( () => {
+      if(data.length === 0) {
+          console.log('the event loop weird')
+      } else {
+          setGraphData({graphData: data})
+          console.log('graph data set??')
+      }
+      // eslint-disable-next-line
+  }, [data, dataLoaded])
 
   let infoData: InfoProps = {
     numPaychecks: 7,
@@ -15,7 +46,7 @@ export default function EssentialBroker(): ReactElement<any, any> {
     <>
       <EssentialInfo {...infoData}/>
       <Space h="xl" />
-      <EssentialDisplays />
+      <EssentialDisplays {...graphData}/>
     </>
   );
 }
--- src/Layouts/EssentialDisplays.tsx
+++ src/Layouts/EssentialDisplays.tsx
@@ -1,7 +1,27 @@
 import { Grid, Paper } from '@mantine/core';
+import { GraphProps } from '../Components/IncomeLineGraph';
+import { RingProps } from '../Components/RingGraph';
 import PayTimeline from '../Components/Timeline';
+import { Categories, formGraphArray, getCurrentSpent, getTotalAmount } from '../Utils/ParsingFunctions';
+import { DisplayData } from './NonessentialDisplays';
+import IncomeLineGraph from '../Components/IncomeLineGraph';
+import RingGraph from '../Components/RingGraph';
 
-export default function EssentialDisplays() {
+export default function EssentialDisplays(props: DisplayData) {
+
+    let graphData: GraphProps = {payPeriods: [], paySpent: []};
+    let ringData: RingProps = {total: 0, spent: 0};
+
+    if(props.graphData) {
+        const linePay = formGraphArray(props.graphData, Categories.Essential, "PaycheckAmount");
+        const lineSpent = formGraphArray(props.graphData, Categories.None, "EssentialSpent");
+        const currSpent = getCurrentSpent(props.graphData, "Essential");
+        const totalAllocation = getTotalAmount(props.graphData);
+
+        graphData = {payPeriods: linePay, paySpent: lineSpent};
+        ringData = {total: totalAllocation, spent: currSpent};
+    }
+
     return (
         <Grid gutter="lg">
             <Grid.Col span={4}>
@@ -9,7 +29,7 @@ export default function EssentialDisplays() {
                 sx={(theme) => ({
                     backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[1]
                 })}>
-                    hi
+                    {props.graphData ? <IncomeLineGraph {...graphData}/> : 'Loading'}
             </Paper>
             </Grid.Col>
             <Grid.Col span={5}>
@@ -17,7 +37,7 @@ export default function EssentialDisplays() {
                 sx={(theme) => ({
                     backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[1]
                 })}>
-                    there
+                    {props.graphData ? <RingGraph {...ringData}/> : 'Loading'}
             </Paper>
             </Grid.Col>
             <Grid.Col span={3}>
