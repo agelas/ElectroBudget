a7249a9d64c917347ffa22e055882257f5190df2
Mathias <mathiaswi177@gmail.com>
2022-09-18 01:18:17 -0700

    Added another function to utils for fetching data from main process

---
+++ src/Utils/FetchData.ts
@@ -0,0 +1,21 @@
+// Get data from main process
+
+export const fetchData = async (setter: React.Dispatch<any>) => {
+    try {
+         let response: any;
+         
+         window.api.requestData('Renderer Requests Data');
+         window.api.localData(function(_event:any, result:any) {
+            response = result;
+            console.log(response);
+            setter(response);
+         })
+         
+        return { success: true, data: response }; 
+    } catch (error) {
+        console.log(error);
+        let response = await fetch('data.json');
+        let json = await response.json();
+        return { success: false, data: json };
+    }
+}
