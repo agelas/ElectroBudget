b47f580c4e66a7e1b73252fba294ebf7c4b5882d
Mathias <mathiaswi177@gmail.com>
2022-12-26 23:41:14 -0800

    Cleanup

--- src/Layouts/NonessentialBroker.tsx
+++ src/Layouts/NonessentialBroker.tsx
@@ -4,11 +4,6 @@ import NonessentialInfo, {InfoProps} from "./NonessentialInfo";
 
 export default function NonEssentialBroker(props: any) {
    
-    /* var length = props.length
-    var discretionary = computeDiscretionaryToDate(props)
-    var available = computeAvailableNow(props)
-    
-    var infoData:InfoProps = {numPaychecks: length, discretionaryToDate: discretionary, availableNow: available} */
     var graphData:DisplayData = {graphData: props}
     let infoData: InfoProps = {
         numPaychecks: 7,
