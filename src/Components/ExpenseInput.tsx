50d977ea0b60295d48c4ad29e090445203ca955c
Mathias <mathiaswi177@gmail.com>
2023-04-11 23:47:24 -0700

    Add interface and adjust component for inputting of new cost items

--- src/Components/ExpenseInput.tsx
+++ src/Components/ExpenseInput.tsx
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextInput, NumberInput, Group, ActionIcon } from "@mantine/core";
 import { SquarePlus } from "tabler-icons-react";
 
-export default function ExpenseInput() {
+export interface ExpenseInputInterface {
+    InputFunction: (newItem: any) => void;
+}
 
-    return (
-        <Group>
-            <TextInput placeholder="Expenditure" />
-            <NumberInput
-                defaultValue={0}
-                parser={(value) => value?.replace(/\$\s?|(,*)/g, '')}
-                formatter={(value) =>
-                    !Number.isNaN(parseFloat(value as string)) ? `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-                        : '$ '
-                }
-            />
-            <ActionIcon color="teal">
-                <SquarePlus />
-            </ActionIcon>
-        </Group>
-    )
+export default function ExpenseInput( {InputFunction}: ExpenseInputInterface) {
+  const [name, setName] = useState("");
+  const [cost, setCost] = useState(0);
+
+  const handleSubmit = () => {
+    const newItem = {
+        Type: "NonEssential",
+        name: name,
+        Cost: cost,
+        Active: true,
+    };
+
+    InputFunction(newItem);
+
+    setName("");
+    setCost(0);
+  }
+
+  return (
+    <Group>
+      <TextInput
+        placeholder="Expenditure"
+        value={name}
+        onChange={(event) => setName(event.currentTarget.value)}
+      />
+      <NumberInput
+        value={cost}
+        parser={(value) => value?.replace(/\$\s?|(,*)/g, "")}
+        formatter={(value) =>
+          !Number.isNaN(parseFloat(value as string))
+            ? `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            : "$ "
+        }
+        onChange={(value) => setCost(value as number)}
+      />
+      <ActionIcon color="teal" onClick={handleSubmit}>
+        <SquarePlus />
+      </ActionIcon>
+    </Group>
+  );
 }
