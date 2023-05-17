f9e28ddba99ea609e53f6ada8084d81731609609
Mathias <mathiaswi177@gmail.com>
2023-05-16 21:40:59 -0700

    Update implementation to account for new GraphProps

--- src/Components/IncomeStackBar.tsx
+++ src/Components/IncomeStackBar.tsx
@@ -1,30 +1,35 @@
-import { FlexibleWidthXYPlot, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, VerticalBarSeries } from "react-vis";
-import '../../node_modules/react-vis/dist/style.css';
+import {
+  FlexibleWidthXYPlot,
+  XAxis,
+  YAxis,
+  VerticalGridLines,
+  HorizontalGridLines,
+  VerticalBarSeries,
+} from "react-vis";
+import "../../node_modules/react-vis/dist/style.css";
 import { GraphProps } from "./IncomeLineGraph";
 
 //At some point maybe add DiscreteColorLegend if it doesn't screw up the formatting too much
 
 export default function IncomeStackBar(props: GraphProps) {
-
-    return (
-        <>
-            <FlexibleWidthXYPlot height={300} xType="ordinal">
-                <VerticalGridLines />
-                <HorizontalGridLines />
-                <XAxis />
-                <YAxis />
-                <VerticalBarSeries
-                    data={props.payPeriods}
-                    barWidth={0.7}
-                    color="#12939A"
-
-                />
-                <VerticalBarSeries
-                    data={props.paySpent}
-                    barWidth={0.7}
-                    color="#79C7E3"
-                />
-            </FlexibleWidthXYPlot>
-        </>
-    )
+  return (
+    <>
+      <FlexibleWidthXYPlot height={300} xType="ordinal">
+        <VerticalGridLines />
+        <HorizontalGridLines />
+        <XAxis />
+        <YAxis />
+        <VerticalBarSeries
+          data={props.payPeriods}
+          barWidth={0.7}
+          color={props.payColor}
+        />
+        <VerticalBarSeries
+          data={props.paySpent}
+          barWidth={0.7}
+          color={props.spentColor}
+        />
+      </FlexibleWidthXYPlot>
+    </>
+  );
 }
