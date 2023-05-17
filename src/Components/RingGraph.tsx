0eea4f092875f6be4dcbd6bfae882e99eba89ef0
Mathias <mathiaswi177@gmail.com>
2023-05-16 21:39:25 -0700

    Update RingProps to include colors fields, enable RingGraph to show different colors

--- src/Components/RingGraph.tsx
+++ src/Components/RingGraph.tsx
@@ -1,26 +1,36 @@
 import { RingProgress, Text, Group } from "@mantine/core";
 
 export interface RingProps {
-    total: number;
-    spent: number;
+  total: number;
+  spent: number;
+  totalColor: string;
+  spentColor: string;
 }
 
 export default function RingGraph(props: RingProps) {
-    return (
-        <Group>
-            <RingProgress
-                size={200}
-                thickness={20}
-                roundCaps
-                label={
-                    <Text size='md' align='center'>Funds Used</Text>
-                }
-                sections={[
-                    { value: ((props.total - props.spent) / props.total * 100), color: 'cyan' },
-                    { value: (props.spent / props.total * 100), color: 'red' }
-                ]}
-            />
-            <Text size="lg"> ${props.spent.toFixed(2)} spent out of ${(props.total).toFixed(2)}</Text>
-        </Group>
-    )
+  return (
+    <Group>
+      <RingProgress
+        size={200}
+        thickness={20}
+        roundCaps
+        label={
+          <Text size="md" align="center">
+            Funds Used
+          </Text>
+        }
+        sections={[
+          {
+            value: ((props.total - props.spent) / props.total) * 100,
+            color: props.totalColor,
+          },
+          { value: (props.spent / props.total) * 100, color: props.spentColor },
+        ]}
+      />
+      <Text size="lg">
+        {" "}
+        ${props.spent.toFixed(2)} spent out of ${props.total.toFixed(2)}
+      </Text>
+    </Group>
+  );
 }
