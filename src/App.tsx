f17c95984355a3b42a62c9610cd0d34a85844beb
Mathias <mathiaswi177@gmail.com>
2023-05-21 15:43:28 -0700

    Add dominant colors to app color theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useState } from 'react';
 import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
-import MainApp from "./Layouts/MainApp";
+import MainApp from './Layouts/MainApp';
 
 export default function App() {
     const [colorScheme, setColorScheme] = useState<ColorScheme>('dark');
@@ -8,7 +8,11 @@ export default function App() {
         setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
     return(
         <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-            <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+            <MantineProvider theme={{ colorScheme, colors: {
+                'nonessential-teal': ['#5fb7bf', '#5fb7bf', '#5fb7bf', '#5fb7bf', '#5fb7bf', '#5fb7bf', '#5fb7bf', '#5fb7bf', '#5fb7bf', '#5fb7bf'],
+                'essential-green': ['#50f15b', '#50f15b', '#50f15b', '#50f15b', '#50f15b', '#50f15b', '#50f15b', '#50f15b', '#50f15b', '#50f15b'],
+                'savings-purple': ['#9b6bf5', '#9b6bf5', '#9b6bf5', '#9b6bf5', '#9b6bf5', '#9b6bf5', '#9b6bf5', '#9b6bf5', '#9b6bf5', '#9b6bf5']
+            } }} withGlobalStyles withNormalizeCSS>
                 <MainApp />
             </MantineProvider>
         </ColorSchemeProvider>
