6da3d0dd734db0b54a54870ed3ac22ba608d590e
Mathias <mathiaswi177@gmail.com>
2023-06-25 17:22:58 -0700

    Run prettier

--- src/Layouts/EssentialBroker.tsx
+++ src/Layouts/EssentialBroker.tsx
@@ -3,20 +3,19 @@ import EssentialInfo, { InfoProps } from "./EssentialInfo";
 import EssentialDisplays from "./EssentialDisplays";
 import { DisplayData } from "./NonessentialDisplays";
 
-export default function EssentialBroker({appData, addExpenseItem}: DisplayData) {
+export default function EssentialBroker({ appData, addExpenseItem }: DisplayData) {
+    var graphData: DisplayData = { appData, addExpenseItem };
+    let infoData: InfoProps = {
+        numPaychecks: 7,
+        discretionaryToDate: 200,
+        availableNow: 1000,
+    };
 
-  var graphData: DisplayData = { appData, addExpenseItem }
-  let infoData: InfoProps = {
-    numPaychecks: 7,
-    discretionaryToDate: 200,
-    availableNow: 1000
-  }
-
-  return (
-    <>
-      <EssentialInfo {...infoData} />
-      <Space h="xl" />
-      <EssentialDisplays {...graphData} />
-    </>
-  );
+    return (
+        <>
+            <EssentialInfo {...infoData} />
+            <Space h='xl' />
+            <EssentialDisplays {...graphData} />
+        </>
+    );
 }
--- src/Layouts/NonessentialBroker.tsx
+++ src/Layouts/NonessentialBroker.tsx
@@ -2,21 +2,19 @@ import { Space } from "@mantine/core";
 import NonessentialDisplays, { DisplayData } from "./NonessentialDisplays";
 import NonessentialInfo, { InfoProps } from "./NonessentialInfo";
 
-export default function NonEssentialBroker({appData, addExpenseItem}: DisplayData) {
-
-    var graphData: DisplayData = { appData, addExpenseItem }
+export default function NonEssentialBroker({ appData, addExpenseItem }: DisplayData) {
+    var graphData: DisplayData = { appData, addExpenseItem };
     let infoData: InfoProps = {
         numPaychecks: 7,
         discretionaryToDate: 200,
-        availableNow: 1000
-    }
-
+        availableNow: 1000,
+    };
 
     return (
         <>
             <NonessentialInfo {...infoData} />
-            <Space h="xl" />
+            <Space h='xl' />
             <NonessentialDisplays {...graphData} />
         </>
-    )
+    );
 }
