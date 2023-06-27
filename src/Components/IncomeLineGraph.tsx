9d2dfcba5a7aab9d8b87d5c321dbdafea84acedd
Mathias <mathiaswi177@gmail.com>
2023-06-26 23:38:05 -0700

    Move interface to Interfaces.tsx

--- src/Components/IncomeLineGraph.tsx
+++ src/Components/IncomeLineGraph.tsx
@@ -1,31 +1,21 @@
 import { FlexibleWidthXYPlot, LineMarkSeries, XAxis, YAxis } from "react-vis";
-
-export interface GraphProps {
-  payPeriods: Array<{ x: number; y: number }>;
-  paySpent: Array<{ x: number; y: number }>;
-  payColor: string;
-  spentColor: string;
-}
+import { GraphProps } from "../Utils/Interfaces";
 
 export default function IncomeLineGraph(props: GraphProps) {
-  var ticks = props.payPeriods.map((t) => t.x);
+    var ticks = props.payPeriods.map((t) => t.x);
 
-  return (
-    <FlexibleWidthXYPlot height={300}>
-      <LineMarkSeries
-        data={props.payPeriods}
-        color={props.payColor}
-        style={{ fill: "none", strokeWidth: 3 }}
-      />
-      <LineMarkSeries
-        data={props.paySpent}
-        color={props.spentColor}
-        curve={"curveMonotoneX"}
-        style={{ fill: "none", strokeWidth: 3 }}
-      />
+    return (
+        <FlexibleWidthXYPlot height={300}>
+            <LineMarkSeries data={props.payPeriods} color={props.payColor} style={{ fill: "none", strokeWidth: 3 }} />
+            <LineMarkSeries
+                data={props.paySpent}
+                color={props.spentColor}
+                curve={"curveMonotoneX"}
+                style={{ fill: "none", strokeWidth: 3 }}
+            />
 
-      <XAxis title="Pay Period" tickValues={ticks} />
-      <YAxis />
-    </FlexibleWidthXYPlot>
-  );
+            <XAxis title='Pay Period' tickValues={ticks} />
+            <YAxis />
+        </FlexibleWidthXYPlot>
+    );
 }
