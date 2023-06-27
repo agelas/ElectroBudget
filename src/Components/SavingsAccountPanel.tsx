5a9cfa8ac1e8e9cb0099420d3c45da9a6210fbdb
Mathias <mathiaswi177@gmail.com>
2023-06-26 23:37:46 -0700

    Move interfaces to Interfaces.ts

--- src/Components/SavingsAccountPanel.tsx
+++ src/Components/SavingsAccountPanel.tsx
@@ -1,33 +1,20 @@
 import { Paper, Text, Title, ActionIcon, Progress, Group, Space } from "@mantine/core";
 import { BuildingBank } from "tabler-icons-react";
-
-export interface SavingsAccountProps {
-    accountHolder: string,
-    accountType: string,
-    goal: number,
-    currentPrincipal: number,
-    key: number
-}
+import { SavingsAccountProps } from "../Utils/Interfaces";
 
 export default function SavingsAccountPanel(props: SavingsAccountProps) {
     return (
-        <Paper radius="md" p="xs" style={{ width: 280 }}>
+        <Paper radius='md' p='xs' style={{ width: 280 }}>
             <Group>
-                <ActionIcon color="lime">
+                <ActionIcon color='lime'>
                     <BuildingBank />
                 </ActionIcon>
-                <Title order={2}>
-                    {props.accountHolder}
-                </Title>
+                <Title order={2}>{props.accountHolder}</Title>
             </Group>
-            <Text size="sm">
-                {props.accountType}
-            </Text>
-            <Space h="sm" />
-            <Text size="xl">
-                ${props.goal} goal
-            </Text>
-            <Progress color="violet" radius="md" size="xl" value={(props.currentPrincipal / props.goal) * 100} />
+            <Text size='sm'>{props.accountType}</Text>
+            <Space h='sm' />
+            <Text size='xl'>${props.goal} goal</Text>
+            <Progress color='violet' radius='md' size='xl' value={(props.currentPrincipal / props.goal) * 100} />
         </Paper>
     );
 }
