394ad74013d44c61405d46ab264487005a346926
Mathias <mathiaswi177@gmail.com>
2023-06-27 22:59:46 -0700

    Verify expense input name is not empty and cost is not 0

--- src/Components/ExpenseInput.tsx
+++ src/Components/ExpenseInput.tsx
@@ -3,47 +3,46 @@ import { TextInput, NumberInput, Group, ActionIcon } from "@mantine/core";
 import { SquarePlus } from "tabler-icons-react";
 import { ExpenseInputInterface } from "../Utils/Interfaces";
 
-export default function ExpenseInput({
-  ExpenseType,
-  InputFunction,
-}: ExpenseInputInterface) {
-  const [name, setName] = useState("");
-  const [cost, setCost] = useState(0);
+export default function ExpenseInput({ ExpenseType, InputFunction }: ExpenseInputInterface) {
+    const [name, setName] = useState("");
+    const [cost, setCost] = useState(0);
 
-  const handleSubmit = () => {
-    const newItem = {
-      Type: ExpenseType,
-      Name: name,
-      Cost: cost,
-      Active: true,
+    const handleSubmit = () => {
+        const newItem = {
+            Type: ExpenseType,
+            Name: name,
+            Cost: cost,
+            Active: true,
+        };
+
+        if (name !== "" && cost !== 0) {
+            InputFunction(newItem);
+        }
+
+        setName("");
+        setCost(0);
     };
 
-    InputFunction(newItem);
-
-    setName("");
-    setCost(0);
-  };
-
-  return (
-    <Group>
-      <TextInput
-        placeholder="Expenditure"
-        value={name}
-        onChange={(event) => setName(event.currentTarget.value)}
-      />
-      <NumberInput
-        value={cost}
-        parser={(value) => value?.replace(/\$\s?|(,*)/g, "")}
-        formatter={(value) =>
-          !Number.isNaN(parseFloat(value as string))
-            ? `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-            : "$ "
-        }
-        onChange={(value) => setCost(value as number)}
-      />
-      <ActionIcon color="teal" onClick={handleSubmit}>
-        <SquarePlus />
-      </ActionIcon>
-    </Group>
-  );
+    return (
+        <Group>
+            <TextInput
+                placeholder='Expenditure'
+                value={name}
+                onChange={(event) => setName(event.currentTarget.value)}
+            />
+            <NumberInput
+                value={cost}
+                parser={(value) => value?.replace(/\$\s?|(,*)/g, "")}
+                formatter={(value) =>
+                    !Number.isNaN(parseFloat(value as string))
+                        ? `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                        : "$ "
+                }
+                onChange={(value) => setCost(value as number)}
+            />
+            <ActionIcon color='teal' onClick={handleSubmit}>
+                <SquarePlus />
+            </ActionIcon>
+        </Group>
+    );
 }
