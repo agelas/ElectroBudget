9b8bb271ceddd2d2e7c0e2e99c88968a04fdc592
Mathias <mathiaswi177@gmail.com>
2023-06-28 22:34:57 -0700

    Make expense group action icon color responsive to expense type

--- src/Components/ExpenseGroup.tsx
+++ src/Components/ExpenseGroup.tsx
@@ -5,53 +5,43 @@ import { ExpenseInputType } from "../Utils/Enums";
 import { ExpenseGroupProps } from "../Utils/Interfaces";
 
 function NonEssentialToggle(props: any) {
-  const [checked, setChecked] = useState<boolean>(Boolean(props.active));
+    const [checked, setChecked] = useState<boolean>(Boolean(props.active));
 
-  return (
-    <Switch
-      checked={checked}
-      onChange={(event) => setChecked(event.currentTarget.checked)}
-      color="cyan"
-      label="Purchased"
-    />
-  );
+    return (
+        <Switch
+            checked={checked}
+            onChange={(event) => setChecked(event.currentTarget.checked)}
+            color='cyan'
+            label='Purchased'
+        />
+    );
 }
 
 function EssentialRecurringToggle() {
-  return <Switch color="cyan" label="Active" />;
+    return <Switch color='cyan' label='Active' />;
 }
 
 export default function ExpenseGroup(props: ExpenseGroupProps) {
-  return (
-    <Paper radius="md" p="xs">
-      <Group position="apart">
-        <Group>
-          <ActionIcon
-            color={
-              props.expenseType === ExpenseInputType.NonEssential
-                ? "#5fb7bf"
-                : "#50f15b"
-            }
-          >
-            {/* NonEssential is coffee, Essential is Lifebuoy because these expenses should be to keep your life afloat */}
-            {props.expenseType === ExpenseInputType.NonEssential ? (
-              <Coffee />
-            ) : (
-              <Lifebuoy />
-            )}
-          </ActionIcon>
-          <Text>{props.name}</Text>
-        </Group>
-        <Group position="right">
-          {props.expenseType === "NonEssential" && (
-            <NonEssentialToggle active={props.active} />
-          )}
-          {props.expenseType === ExpenseInputType.Essential && (
-            <EssentialRecurringToggle />
-          )}
-          <Text>$ {props.cost}</Text>
-        </Group>
-      </Group>
-    </Paper>
-  );
+    return (
+        <Paper radius='md' p='xs'>
+            <Group position='apart'>
+                <Group>
+                    <ActionIcon>
+                        {/* NonEssential is coffee, Essential is Lifebuoy because these expenses should be to keep your life afloat */}
+                        {props.expenseType === ExpenseInputType.NonEssential ? (
+                            <Coffee color='#5fb7bf' />
+                        ) : (
+                            <Lifebuoy color='#50f15b' />
+                        )}
+                    </ActionIcon>
+                    <Text>{props.name}</Text>
+                </Group>
+                <Group position='right'>
+                    {props.expenseType === "NonEssential" && <NonEssentialToggle active={props.active} />}
+                    {props.expenseType === ExpenseInputType.Essential && <EssentialRecurringToggle />}
+                    <Text>$ {props.cost}</Text>
+                </Group>
+            </Group>
+        </Paper>
+    );
 }
