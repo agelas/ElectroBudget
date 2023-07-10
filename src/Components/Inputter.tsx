551f7b7d95287f655a4e931a360913397427cf64
Mathias <mathiaswi177@gmail.com>
2023-07-09 22:01:45 -0700

    Prettier

--- src/Components/Inputter.tsx
+++ src/Components/Inputter.tsx
@@ -3,23 +3,20 @@ import { CirclePlus, CircleMinus } from "tabler-icons-react";
 import ExpenseInput from "./ExpenseInput";
 import { IExpenseInput } from "../Utils/Interfaces";
 
-export default function Inputter({
-  ExpenseType,
-  InputFunction,
-}: IExpenseInput) {
-  return (
-    <>
-      <Divider my="xs" label="Add Items" />
-      <ExpenseInput ExpenseType={ExpenseType} InputFunction={InputFunction} />
-      <Divider my="xs" label="Paychecks" />
-      <Center>
-        <ActionIcon color="green" size="xl">
-          <CirclePlus />
-        </ActionIcon>
-        <ActionIcon color="red" size="xl">
-          <CircleMinus />
-        </ActionIcon>
-      </Center>
-    </>
-  );
+export default function Inputter({ ExpenseType, InputFunction }: IExpenseInput) {
+    return (
+        <>
+            <Divider my='xs' label='Add Items' />
+            <ExpenseInput ExpenseType={ExpenseType} InputFunction={InputFunction} />
+            <Divider my='xs' label='Paychecks' />
+            <Center>
+                <ActionIcon color='green' size='xl'>
+                    <CirclePlus />
+                </ActionIcon>
+                <ActionIcon color='red' size='xl'>
+                    <CircleMinus />
+                </ActionIcon>
+            </Center>
+        </>
+    );
 }
