2eff0c64e70838f47df62777344c8ac30fcd78fa
Mathias <mathiaswi177@gmail.com>
2023-07-09 22:14:12 -0700

    Add space for account creator

--- src/Layouts/SavingsDisplays.tsx
+++ src/Layouts/SavingsDisplays.tsx
@@ -4,28 +4,45 @@ import SavingsAccounts from "../Components/SavingsAccounts";
 import { ISavingsData } from "../Utils/Interfaces";
 
 export default function SavingsDisplays({ accountsData }: ISavingsData) {
-    const dominantColor: string = 'savings-purple';
+    const dominantColor: string = "savings-purple";
 
     return (
-        <Grid gutter="lg">
+        <Grid gutter='lg'>
             <Grid.Col span={9}>
-                <Paper radius="md" p="md"
+                <Paper
+                    radius='md'
+                    p='md'
                     sx={(theme) => ({
-                        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[1]
-                    })}>
+                        backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[1],
+                    })}
+                >
                     <Center>
-                        <SavingsAccounts accountsData={accountsData}/>
+                        <SavingsAccounts accountsData={accountsData} />
                     </Center>
                 </Paper>
             </Grid.Col>
             <Grid.Col span={3}>
-                <Paper radius="md" p="md"
+                <Paper
+                    radius='md'
+                    p='md'
                     sx={(theme) => ({
-                        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[1]
-                    })}>
+                        backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[1],
+                    })}
+                >
                     <PayTimeline lineColor={dominantColor} />
                 </Paper>
             </Grid.Col>
+            <Grid.Col span={4}>
+                <Paper
+                    radius='md'
+                    p='md'
+                    sx={(theme) => ({
+                        backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[1],
+                    })}
+                >
+                    
+                </Paper>
+            </Grid.Col>
         </Grid>
-    )
+    );
 }
