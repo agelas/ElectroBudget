e706c78dbd9b2b5f135cb42d6214378ad1193e89
Mathias <mathiaswi177@gmail.com>
2023-07-16 23:14:47 -0700

    Add saveData to context bridge

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,9 +1,9 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
+const { contextBridge, ipcRenderer } = require("electron");
 
 const API = {
     localData: () => new Promise((resolve) => ipcRenderer.once("local-Data", (_event, data) => resolve(data))),
     requestData: (msg) => ipcRenderer.send("request-Data", msg),
-  };
+    saveData: (data) => ipcRenderer.invoke("saveData", data),
+};
 
 contextBridge.exposeInMainWorld("api", API);
